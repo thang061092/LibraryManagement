6fcf75454bee5bbad66fd0bea8a6ada6571f9555
thang061092 <buimanhthangtb@gmail.com>
2020-06-11 10:47:40 +0700

    fixCss

--- src/View/core/menu.php
+++ src/View/core/menu.php
@@ -1,5 +1,5 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">LIBRARY</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light ">
+    <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-book"></span> LIBRARY</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -8,25 +8,26 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="index.php?page=view-category"><span class="glyphicon glyphicon-home"></span> Trang chủ</a>
+                <a class="nav-link" href="index.php?page=view-category"><span class="glyphicon glyphicon-home"></span>
+                    Trang chủ</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="#"></a>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
                    aria-haspopup="true" aria-expanded="false">
-                    Menu
+                    <span class="glyphicon glyphicon-list-alt"></span>  Menu
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="index.php?page=view-category">Xem danh mục sách</a>
-                    <a class="dropdown-item" href="index.php?page=view-book">Xem Sách</a>
+                    <a class="dropdown-item" href="index.php?page=view-category"><span class="glyphicon glyphicon-hand-right"></span> Xem danh mục sách</a>
+                    <a class="dropdown-item" href="index.php?page=view-book"><span class="glyphicon glyphicon-hand-right"></span> Xem Sách</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                    <a class="dropdown-item" href="index.php?page=view-student"><span class="glyphicon glyphicon-hand-right"></span> Danh Sách Sinh Viên</a>
                 </div>
             </li>
             <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                <a class="nav-link " href="#" tabindex="-1" aria-disabled="false"><span class="glyphicon glyphicon-envelope"></span> Góp ý</a>
             </li>
         </ul>
     </div>
