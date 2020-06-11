16c86d61623c1f3852d0668848b8f2e5b55806fb
thang061092 <buimanhthangtb@gmail.com>
2020-06-11 11:20:33 +0700

    fixB

--- src/View/students/update.php
+++ src/View/students/update.php
@@ -4,17 +4,17 @@
     <div>
         <form method="post" action="index.php?page=update-student">
             <label for="exampleFormControlTextarea1">Id: </label>
-            <input class="form-control" type="text" name="id" value="<?php echo $student->getId(); ?>" required>
+            <input class="form-control" type="hidden" name="id" value="<?php echo $student->getId(); ?>" disabled>
             <label for="exampleFormControlTextarea1">Student Name: </label>
-            <input class="form-control" type="text" name="studentName" value="<?php echo $student->getStudentName(); ?>" required>
+            <input class="form-control" type="text" name="studentName" value="<?php echo $student->getStudentName(); ?>" >
             <label for="exampleFormControlTextarea1">Gender: </label>
-            <input class="form-control" type="text" name="gender" value="<?php echo $student->getGender(); ?>" required>
+            <input class="form-control" type="text" name="gender" value="<?php echo $student->getGender(); ?>" >
             <label for="exampleFormControlTextarea1">Address: </label>
-            <input class="form-control" type="text" name="address" value="<?php echo $student->getAddress(); ?>" required>
+            <input class="form-control" type="text" name="address" value="<?php echo $student->getAddress(); ?>" >
             <label for="exampleFormControlTextarea1">Email: </label>
-            <input class="form-control" type="text" name="email" value="<?php echo $student->getEmail(); ?>" required>
+            <input class="form-control" type="text" name="email" value="<?php echo $student->getEmail(); ?>" >
             <label for="exampleFormControlTextarea1">Phone: </label>
-            <input class="form-control" type="text" name="phone" value="<?php echo $student->getPhone(); ?>" required>
+            <input class="form-control" type="text" name="phone" value="<?php echo $student->getPhone(); ?>" >
             <div>
                 <button class="btn btn-primary" type="submit">Update</button>
                 <button class="btn btn-secondary" onclick="window.history.go(-1); return false;">Cancel</button>
